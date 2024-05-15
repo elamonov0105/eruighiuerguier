376683daf51401361ee1a3357dea160770ae0de0
EZ7-easy <b.sh.tutor@gmail.com>
2024-05-15 11:36:10 +0500

    kodlar.js

---
+++ uyIshi.js
@@ -0,0 +1,51 @@
+'use strect'
+// const Kondetsaner = {
+//     size: 40,
+//     color: "black",
+//     isConnectable: function (){
+//         console.log("connectable");
+//     }
+// }
+// //
+// const artelTv ={
+//     isSmartTv: false
+// }
+//
+//
+//  artelTv.__proto__ = Kondetsaner
+// console.log(Kondetsaner);
+
+// const kamputer = {
+//     rangi:'matviy',
+//     xotira: 20,
+//     narsalar: 'toliq',
+// }
+//
+// const noutbuk = {
+//     displey: 20,
+//     rangi: 'sariq'
+// }
+// Object.setPrototypeOf(kamputer, noutbuk);
+//
+// console.log(noutbuk);
+
+//
+// const ekran = Object.create('kamputer');
+// // //
+// console.log(ekran)
+
+// let x = 9
+// let y = x
+// //
+// y= y+9
+// //
+// console.log(x)
+// console.log(y)
+
+// const number ={
+//     x: 57,
+//     y: 10
+// }
+// const newNumber = number
+//
+// newNumber.x = 10
